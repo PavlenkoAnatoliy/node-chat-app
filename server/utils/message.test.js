2b85a585e40f0e8f9778deef81f627abf43fc467
Tolik <Tolik@MacBook-Pro-Tolik.local>
2018-07-29 20:55:17 +0300

    Add css for chat page

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -29,10 +29,3 @@ describe('generateLocationMessage', () => {
     expect(locMessage).toMatchObject({ from, url });
   });
 });
-// var generateLocationMessage = (from, latitude, longitude) => {
-//   return {
-//     from,
-//     url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-//     createdAt: new Date().getTime()
-//   };
-// };
