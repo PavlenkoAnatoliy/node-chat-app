862611cd9a7def66d4a9eac0520a2fe9c1bc108e
Tolik <Tolik@MacBook-Pro-Tolik.local>
2018-07-30 02:54:15 +0300

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,8 @@
 var socket = io();
 
 function scrollToBottom() {
-  // Selections
   var messages = $('#messages');
   var newMessage = messages.children('li:last-child');
-  //Heights
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
@@ -18,11 +16,30 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = $.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+})
+
+socket.on('updateUserList', function (users) {
+  var ol = $('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append($('<li></li>').text(user));
+  });
+
+  $('#users').html(ol);
 })
 
 socket.on('newMessage', function (message) {
