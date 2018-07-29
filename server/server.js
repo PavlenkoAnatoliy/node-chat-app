ccdfd56a322f2cf3f75602301e92d5eab46bf029
Tolik <Tolik@MacBook-Pro-Tolik.local>
2018-07-29 12:58:15 +0300

    Great new user, and alert others

--- server/server.js
+++ server/server.js
@@ -11,20 +11,37 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello world');
-// })
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined chat app',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('Create message ', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
   });
+
+  socket.on('greeting', (text) => {
+    console.log(text);
+  })
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
